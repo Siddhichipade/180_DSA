5fd2548ddc434b23d948e2b3443eed276f73d96a
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-21 23:23:12 +0530

    instance variable

---
+++ Day-2/Instvab.java
@@ -0,0 +1,12 @@
+class Instvab 
+{
+    public String geek;
+    {
+        this.geek = "Siddhi Chipade";
+    }
+    public static void main(String[] args)
+    {
+        Instvab name = new Instvab();
+        System.out.println("Geek name is: " + name.geek);
+    }
+}
