d5fe22d66cd4f8ee8816b301cd6adca0de2f9e5b
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-21 23:29:25 +0530

    Static variable

---
+++ Day-2/Varstatic.java
@@ -0,0 +1,9 @@
+class Varstatic 
+{
+    public static String geek = "Siddhi Chipade";
+  
+    public static void main(String[] args)
+    {
+        System.out.println("Geek Name is : " + GFG.geek);
+    }
+}
