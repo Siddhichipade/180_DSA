1979c2a5c2048393d335f0e256b24773a25890bf
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-21 18:35:10 +0530

    day 2

---
+++ Day-2/PrimeNumber.java
@@ -0,0 +1,24 @@
+public class PrimeNumber
+{
+    public static void main(String[]args)
+    {
+        System.out.print("Prime Number Between 1 to 100 are:");
+
+        for(int num = 2; num <= 100; num++)
+        {
+            boolean isPrime = true;
+            for(int i =2; i<= num / 2; i++)
+            {
+                if(num%i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                System.out.print(num+ " ");
+            }
+        }
+    }
+}
