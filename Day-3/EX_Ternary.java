c4a28d4c091c7bae692f922c5a9d36554d765f6a
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-22 21:06:15 +0530

    use ternory operator rather than if else statement

---
+++ Day-3/EX_Ternary.java
@@ -0,0 +1,10 @@
+public class EX_Ternary
+{
+  public static void main(String[] args) 
+  {   
+    int time = 20;
+    String result;
+    result = (time < 18) ? "Good day." : "Good evening.";
+    System.out.println(result);
+  }
+}
