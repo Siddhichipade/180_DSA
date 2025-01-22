18f6be29fe75298fd49e1e619e7a4cc6c330309c
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-22 08:36:39 +0530

    Scope variable static, instance,local and also using for loop

---
+++ Day-3/Example_instance.java
@@ -0,0 +1,9 @@
+public class Example_instance
+{
+    int age = 21;
+    public static void main(String[]args)
+    {
+        Example_instance obj1 = new Example_instance();
+        System.out.println(obj1.age);
+    }
+}
---
+++ Day-3/scopestatic.java
@@ -0,0 +1,8 @@
+public class scopestatic
+{
+    static int count = 4;
+    public static void main(String[]args)
+    {
+        System.out.println(count);
+    }
+}
