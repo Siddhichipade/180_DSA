52d698de6d7012131596321321596fdf6d9fc4df
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-23 21:06:40 +0530

    loops in java

---
+++ Day-4/EX_For.java
@@ -0,0 +1,11 @@
+public class EX_For
+ {
+    public static void main(String args[]) 
+    {
+        for (int i = 1; i <= 5; i++) 
+        { 
+            System.out.println("" + i);
+        }
+        System.out.println("Loop has ended.");
+    }
+}
---
+++ Day-4/ExBreak.java
@@ -0,0 +1,14 @@
+public class ExBreak
+{
+    public static void main(String[]args)
+    {
+        int i = 1;
+        do
+        {
+            System.out.println(i);
+            i++;
+            if(i == 5)
+            break;
+        }while(i <= 10);
+    }
+}
