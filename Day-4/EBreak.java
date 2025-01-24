82d3aac36342920da55ea955a293cfc507ef9c90
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-24 10:20:55 +0530

    break and continue statement

---
+++ Day-4/EBreak.java
@@ -0,0 +1,18 @@
+class EBreak 
+{  
+    public static void main (String[] args) 
+    {
+      int n = 1;
+      switch(n)
+       {
+        case 1: 
+          System.out.println("First Case");
+          break;
+        case 2:
+          System.out.println("Second Case");
+          break;
+        default:
+          System.out.println("default case");
+      }
+    }
+}
