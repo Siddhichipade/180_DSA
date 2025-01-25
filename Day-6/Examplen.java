8bfa9fd4e70c206bd93598d779d2680e0afbef69
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-25 18:44:00 +0530

    calling function

---
+++ Day-6/Examplen.java
@@ -0,0 +1,12 @@
+public class Examplen
+ {
+    public static int add(int a, int b)
+     {
+        return a + b; // Returns the sum of a and b
+     }
+
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println("Sum: " + result); // Output: Sum: 8
+    }
+}
---
+++ Day-6/ExcallFun.java
@@ -0,0 +1,13 @@
+class ExcallFun 
+{
+    // User-defined method
+    void hello() {
+        System.out.println("This is a user-defined method.");
+    }
+    public static void main(String[] args) {
+        // Create an object
+        ExcallFun obj = new ExcallFun(); 
+        // Call the method
+        obj.hello();          
+    }
+}
