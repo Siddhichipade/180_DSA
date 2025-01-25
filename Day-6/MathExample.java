e00253807f9101931cedbbf2017e0c1b8d597ced
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-25 18:53:06 +0530

    predifined function

---
+++ Day-6/MathExample.java
@@ -0,0 +1,9 @@
+public class MathExample 
+{
+    public static void main(String[] args) 
+    {
+        double squareRoot = Math.sqrt(16); 
+        System.out.println("Square Root: " + squareRoot);
+
+    }
+}
