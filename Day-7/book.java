0a023795133ece87e26529b02f42cb93c941834d
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-26 19:40:36 +0530

    Calling  Function using object

---
+++ Day-7/book.java
@@ -0,0 +1,13 @@
+//calling function using object
+public class book
+{
+    public static void note()
+    {
+System.out.println("books feel like home");
+    }
+    public static void main(String[]args)
+    {
+        book b = new book();
+        b.note();
+    }
+}
