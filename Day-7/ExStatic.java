5667c12529c61b0ab3863f7a430b93d9e596c4df
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-26 19:21:21 +0530

    Static Function

---
+++ Day-7/ExStatic.java
@@ -0,0 +1,18 @@
+// Demonstration of the static method  
+
+class ExStatic 
+{
+  // static method 
+  public static void greet()
+  {
+    System.out.println("Hello Geek!");
+  }
+    public static void main (String[] args) {
+      
+       // calling the method directily
+       greet();
+        // calling the method 
+        // using the class name
+        ExStatic.greet();  
+    }
+}
