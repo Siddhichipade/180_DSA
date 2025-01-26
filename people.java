5929467d0531cfeb98530cb9cf196b6c31a9da15
Siddhichipade <chipadesiddhi@gmail.com>
2025-01-26 19:35:37 +0530

    Static Function example 2

---
+++ people.java
@@ -0,0 +1,12 @@
+public class people
+{
+    public static void good()
+    {
+        System.out.println("there are so many good people out there");
+    }
+    public static void main(String[]args)
+    {
+        good();
+        people.good();
+    }
+}
