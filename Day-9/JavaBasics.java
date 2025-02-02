7eeab60d83750a963de557c515601e407e55e20e
Siddhichipade <chipadesiddhi@gmail.com>
2025-02-02 22:37:22 +0530

    java basic

---
+++ Day-9/JavaBasics.java
@@ -0,0 +1,15 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Sum: " + (a + b));  
+        System.out.println("Difference: " + (a - b));  
+        System.out.println("Product: " + (a * b));  
+        System.out.println("Division: " + (a / b));  
+        System.out.println("Remainder: " + (a % b));  
+
+        boolean isGreater = a > b;
+        System.out.println("Is a greater than b? " + isGreater);
+    }
+}
