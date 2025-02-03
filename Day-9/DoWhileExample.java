a556b38fd50984de9d344c4c3459cc2085a9b9fa
Siddhichipade <chipadesiddhi@gmail.com>
2025-02-03 09:06:37 +0530

    java basic

---
+++ Day-9/DoWhileExample.java
@@ -0,0 +1,12 @@
+public class DoWhileExample
+ {
+    public static void main(String[] args)
+     {
+        int num = 1;
+        do
+         {
+            System.out.println("Number: " + num);
+            num++;
+        } while (num <= 5);
+    }
+}
